b8015d87277c80039a9d2082e57c4eb686b1a08c
Amadeo Huamanculi <amadeus20@outlook.com.pe>
2020-09-27 13:41:45 -0500

    AppMislentes2.0

---
+++ app/src/main/java/com/amadeo/appmislentes/Vista/ViewAdicionales/TamanoActivity.java
@@ -0,0 +1,17 @@
+package com.amadeo.appmislentes.Vista.ViewAdicionales;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.amadeo.appmislentes.R;
+
+public class TamanoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tamano);
+
+    }
+}
