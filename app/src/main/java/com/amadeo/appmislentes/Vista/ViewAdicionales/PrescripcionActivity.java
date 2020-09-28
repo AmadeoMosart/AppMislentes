893267e9df8cb8aa6d54632735aee58afdf4b684
Amadeo Huamanculi <amadeus20@outlook.com.pe>
2020-09-27 19:52:12 -0500

    AppMislentes2.1

--- app/src/main/java/com/amadeo/appmislentes/Vista/ViewAdicionales/PrescripcionActivity.java
+++ app/src/main/java/com/amadeo/appmislentes/Vista/ViewAdicionales/PrescripcionActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.amadeo.appmislentes.R;
 
@@ -27,6 +28,7 @@ Button btnPrescription;
      switch (v.getId()){
          case R.id.btnPrescription:
              Intent intent = new Intent(PrescripcionActivity.this, TipoLenteActivity.class);
+             Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
              startActivity(intent);
              break;
      }
