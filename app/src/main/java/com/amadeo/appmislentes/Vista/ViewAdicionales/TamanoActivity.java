777ce8fb961bccfd367ce4395e3216ab542220b7
Amadeo Huamanculi <amadeus20@outlook.com.pe>
2020-09-27 19:54:32 -0500

    AppMislentes2.3

--- app/src/main/java/com/amadeo/appmislentes/Vista/ViewAdicionales/TamanoActivity.java
+++ app/src/main/java/com/amadeo/appmislentes/Vista/ViewAdicionales/TamanoActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.amadeo.appmislentes.R;
 
@@ -27,6 +28,7 @@ public class TamanoActivity extends AppCompatActivity implements View.OnClickLis
         switch (v.getId()){
             case R.id.btnTamañoSiguiente:
                 Intent intent=new Intent(TamanoActivity.this,PrescripcionActivity.class);
+                Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
                 startActivity(intent);
                 break;
         }
